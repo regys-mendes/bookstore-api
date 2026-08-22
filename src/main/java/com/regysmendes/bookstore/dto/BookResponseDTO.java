358009b293ddb18315862a1cf07dc926640fcae6
regys-mendes <regyseduardomendes123@gmail.com>
2026-08-22 19:53:29 -0300

    feat: created BookResponseDTO

---
+++ src/main/java/com/regysmendes/bookstore/dto/BookResponseDTO.java
@@ -0,0 +1,76 @@
+package com.regysmendes.bookstore.dto;
+
+import com.regysmendes.bookstore.entities.Book;
+import com.regysmendes.bookstore.entities.BookStatus;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class BookResponseDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDate year;
+
+    private BookStatus status;
+
+    public BookResponseDTO(){
+    }
+
+    public BookResponseDTO(Long id, String title, String author, LocalDate year, BookStatus status) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getYear() {
+        return year;
+    }
+
+    public void setYear(LocalDate year) {
+        this.year = year;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BookResponseDTO book = (BookResponseDTO) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
